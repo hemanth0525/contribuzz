a3975d98f71070d4a9d4982135d8fd6ec7e7e966
HEMANTH M <128402149+hemanth0525@users.noreply.github.com>
2024-10-29 20:13:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import Hero from '@/components/Hero'
-import Sidebar from '@/components/Sidebar'
+// import Sidebar from '@/components/Sidebar'
 import HowItWorks from '@/components/HowItWorks'
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-grow">
-        <Sidebar />
         <main className="flex-grow">
           <Hero />
           <HowItWorks />
