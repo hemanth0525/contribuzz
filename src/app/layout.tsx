6b64da929680d39bc2e56af3ed2c58e97bc09fc9
Hemanth M <hemanth00405@gmail.com>
2024-11-01 20:26:43 +0530

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({
 })
 
 export const metadata = {
-  title: 'Contri.buzz - Celebrate Your Open Source Contributors with an Interactive Wall',
+  title: 'Contri.buzz - Celebrate Your Open Source Contributors',
   description: "Showcase a visually stunning contributors' wall in your GitHub README.md / App. Motivate your contributors by highlighting their valuable contributions to your open source projects, fostering community engagement and recognition. Join us in celebrating the unsung heroes of the open-source world and make your project shine!",
   keywords: 'Open Source, Contributors, GitHub, Recognition, Community Engagement, Developer Motivation, Contributors Wall, Open Source Contributions, Celebrate Contributors',
   author: 'Hemanth M',
@@ -23,25 +23,9 @@ export const metadata = {
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  // Add the Google Analytics tracking ID
-  const GA_TRACKING_ID = 'G-SVLY4BRHRQ';
 
   return (
     <html lang="en" className={inter.variable}>
-      <head>
-        {/* Google Tag (gtag.js) */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });
-            `,
-          }}
-        />
-      </head>
       <body>
         <div className="scroll-container">
           {children}
